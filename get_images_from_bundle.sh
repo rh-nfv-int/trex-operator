af18698cfdc15baad4274c670f06e01f6117ca62
Tatiana Krishtop <tkrishto@redhat.com>
2023-02-01 18:30:03 +0100

    Add correct digests for the server and for the app

--- get_images_from_bundle.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-blobs=$(opm render quay.io/rh-nfv-int/trex-operator-bundle:v0.2.12 \
-| jq -r '.properties[] | select(.type == "olm.bundle.object") | .value.data')
-for blob in $blobs
-do
-	echo "$blob" \
-    | base64 --decode \
-    | jq -r '.spec.install.spec | select(.deployments != null).deployments[].spec.template.spec.containers[].image'
-done
